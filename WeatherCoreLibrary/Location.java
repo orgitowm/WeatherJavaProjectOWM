b70d28c1abc808e736c6f70f835bd19921c66691
orgitowm <orowmapi@gmail.com>
2015-09-06 20:13:53 +0300

    Create Location.java

---
+++ WeatherCoreLibrary/Location.java
@@ -0,0 +1,11 @@
+package WeatherCoreLibrary;
+
+// Define Location class that used as a data type
+public class Location {
+	String cityName;
+
+	public Location(String cityName) {
+		super();
+		this.cityName = cityName;
+	}
+}
